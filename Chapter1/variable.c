92d70209122d91347d5174072736c362235d0bfd
WayneMing <badmanweimin@gmail.com>
2024-11-06 21:50:34 +0800

    变量和常量的概念

--- Chapter1/variable.c
+++ Chapter1/variable.c
@@ -1,4 +1,16 @@
+/**
+ * 大纲
+ * 1. 变量和常量的概念
+ *	1.1 变量可分为局部变量和全局变量
+ *	1.2 全局变量和局部变量的变量名可以相同，相同情况下局部优先。
+ *	1.3 建议不要将全局变量和局部变量的变量名相同
+ * 2. scanf()输入函数
+ */
+
 #include <stdio.h>
+
+// 全局变量
+int b = 20;
 
 int main() {
 	// 整形
@@ -9,7 +21,25 @@ int main() {
 	char char1 = 'W';
 	printf("Variable char1: %c\n", char1);
 
-	// 变量
+	// 局部变量
+	short age = 44;
+	int high = 180;
+	float weight = 88.5;
+	int b = 30;
 
+	/**
+	 * 计算两个整数的和
+	 */
+	int num1 = 0;	// 初始化变量，最好给个0如果不给编译器会警告。如果不初始化会给随机值
+	int num2 = 0;
+
+	// 输入2个整数
+	scanf("%d %d", &num1, &num2);
+
+	// 求和
+	int sum = num1 + num2;
+
+	// 输出
+	printf("Sum: %d\n", sum);
 	return 0;
 }
