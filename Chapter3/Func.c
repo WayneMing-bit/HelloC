e28a8508e3b68146943e3e51c1ebfc57c3fe7d47
Wayne Ming <badmanweimin@gmail.com>
2024-11-19 10:01:01 +0800

    素数

--- Chapter3/Func.c
+++ Chapter3/Func.c
@@ -1,14 +1,58 @@
+#include <math.h>
 #include "stdio.h"
 #include "string.h"
 
-int get_max(int num1, int num2) {
-    return num1 > num2 ? num1 : num2;
-}
+int get_max(int num1, int num2) { return num1 > num2 ? num1 : num2; }
 
-void Swap(int * a, int * b) {
+// 变量交换
+void Swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+// 素数实现方式1
+void PrimeNumber1() {
+    int count = 0;
+    for (int i = 100; i <= 200; i++) {
+        int flag = 1;   // flag=1表示素数
+
+        for (int j = 2; j <= i - 1; j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag == 1) {
+            count++;
+            printf("%d is prime number. \n", i);
+        }
+
+    }
+    printf("count = %d\n", count);
+}
+
+// 素数实现方式2
+void PrimeNumber2() {
+    int count = 0;
+    for (int i = 100; i <= 200; i++) {
+        int flag = 1;   // flag=1表示素数
+
+        for (int j = 2; j <= sqrt(i); j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag == 1) {
+            count++;
+            printf("%d is prime number. \n", i);
+        }
+
+    }
+    printf("count = %d\n", count);
 }
 
 int main() {
@@ -22,7 +66,7 @@ int main() {
 
     int a = 0;
     int b = 0;
-//    scanf("%d %d", &a, &b);
+    //    scanf("%d %d", &a, &b);
     int m = get_max(a, b);
     printf("%d\n", m);
 
@@ -30,5 +74,8 @@ int main() {
     int b2 = 4;
     Swap(&a1, &b2);
     printf("%d, %d", a1, b2);
+
+    PrimeNumber1();
+    PrimeNumber2();
     return 0;
 }
