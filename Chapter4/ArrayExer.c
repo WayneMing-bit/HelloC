0bc0944686638db6f8659f37f459b1c47db0319e
Wayne Ming <badmanweimin@gmail.com>
2024-11-24 22:13:41 +0800

    二维数组练习

--- Chapter4/ArrayExer.c
+++ Chapter4/ArrayExer.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 int getMaxValue(int arr[], int size) {
-    int temp = 0;
-    for (int i = 0; i < size; i++) {
+    int temp = arr[0];
+    for (int i = 1; i < size; i++) {
         if (arr[i] > temp) {
             temp = arr[i];
         }
@@ -13,12 +13,50 @@ int getMaxValue(int arr[], int size) {
     return temp;
 }
 
+int getMinValue(int arr[], int size) {
+    int temp = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < temp) {
+            temp = arr[i];
+        }
+    }
+
+    return temp;
+}
+
+int getAvgValue(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    return sum / size;
+}
+
+int getSumValue(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
 int main() {
     int arr[10] = {34, 54, 2, 32, 54, 57, 3, 32, 87, 43};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     int maxValue = getMaxValue(arr, size);
     printf("Arr Max Value: %d\n", maxValue);
+
+    int minValue = getMinValue(arr, size);
+    printf("Arr Max Value: %d\n", minValue);
+
+    int avgValue = getAvgValue(arr, size);
+    printf("Arr Max Value: %d\n", avgValue);
+
+    int sumValue = getSumValue(arr, size);
+    printf("Arr Max Value: %d\n", sumValue);
 
     return 0;
 }
