c2a0457071f7364a6fa5ff96314fe4e712a5b6cb
Wayne Ming <badmanweimin@gmail.com>
2024-12-03 14:10:32 +0800

    结构体嵌套 单向链表、二叉树结构

--- Chapter7/03_StructTest.c
+++ Chapter7/03_StructTest.c
@@ -15,6 +15,17 @@ struct Student {
     char gender;
 } stu1;
 
+typedef struct Node {
+    int data;
+    struct Node *next;
+} LNode;
+
+
+typedef struct BTNode {
+    int data;
+    struct BTNode *lchild;
+    struct BTNode *rchild;
+} BTNode;
 
 int main() {
     strcpy(stu1.name.firstName, "Ming");
@@ -23,6 +34,17 @@ int main() {
     // or
     struct Name myName = {"Ming", "Wayne"};
     stu1.name = myName;
+
+    // 单向链表
+    LNode node1 = {.data = 1};
+    LNode node2 = {.data = 2, .next = &node1};
+
+    // 二叉树结构
+    BTNode btNode1 = {.data = 1};
+    BTNode btNode2 = {.data = 2, .lchild = &btNode1};
+    BTNode btNode3 = {.data = 3, .lchild = &btNode2, .rchild = &btNode1};
+
+    int btNode3Data = btNode1.lchild->lchild->data;
 
     return 0;
 }
